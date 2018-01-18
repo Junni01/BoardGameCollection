c4f78f92db0c401d6eaf4daa2838d9af2a332f46
Saku Junni <junnisaku@gmail.com>
2018-01-18 10:05:53 +0200

    Setting up the azure

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 3000;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Hello world! My name is Saku, and this is my first Node.js server");
