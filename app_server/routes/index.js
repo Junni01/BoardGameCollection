54146bb2813f508772003dfc9970b4c7add8699c
Saku Junni <junnisaku@gmail.com>
2018-02-01 10:26:34 +0200

    aha

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlMain = require('../controllers/main');
+const ctrlMain = require('../controllers/main');
+const ctrlConnection = require('../controllers/ctrlConnection');
+const ctrlUpdate = require('../controllers/ctrlUpdate');
 /* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/collection');
+router.get('/update');
+
 
 module.exports = router;
