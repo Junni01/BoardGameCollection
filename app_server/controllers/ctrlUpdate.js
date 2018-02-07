11d929c0fade872afd97082863d9d1556d90a53d
Saku Junni <junnisaku@gmail.com>
2018-02-07 15:27:02 +0200

    three page

--- app_server/controllers/ctrlUpdate.js
+++ app_server/controllers/ctrlUpdate.js
@@ -1,5 +1,12 @@
 const update = function(req, res) {
-    res.render('update');
+    res.render('update', { gameLog:
+        [
+
+            { game:'Catan', datePlayed:'11-01-2018', won:'Yes'},
+            { game:'Carcassonne', datePlayed:'01-02-2018', won:'No'},
+            { game:'Scythe', datePlayed:'21-01-2018', won:'Yes'},
+            { game:'Eclipse', datePlayed:'5-01-2018', won:'No'}
+        ]});
 };
 
 module.exports = { update };
