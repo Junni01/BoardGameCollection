e3264cbaacf6337aeccd3d0e6cb9875b72238d21
Saku Junni <junnisaku@gmail.com>
2018-03-15 10:32:19 +0200

    API EXPOSED!

--- app_api/controllers/ctrlApiCollection.js
+++ app_api/controllers/ctrlApiCollection.js
@@ -3,15 +3,28 @@ const mongoose = require('mongoose');
 const collectionModel = mongoose.model('game');
 
 const collectionList = function (req, res) {
-  res
-      .status(200)
-      .json({'name' : 'rating'});
+  collectionModel.find({}, function (err, games) {
+      if (err) {
+          res.status(404).json(err);
+      }
+      else {
 
+          res.status(200).json(games);
+      }
+
+      });
 };
 
 const addGame = function (req, res) {
 
-    res.status(201).json({"add game to collection" : "in progress"});
+     collectionModel.create(req.body, function(err, newGame) {
+         if(err) {
+             res.status(400).json(err);
+         }
+         else {
+             res.status(201).json(newGame);
+         }
+         });
 };
 
 module.exports = {
--- app_api/controllers/ctrlApiPlays.js
+++ app_api/controllers/ctrlApiPlays.js
@@ -3,15 +3,29 @@ const mongoose = require('mongoose');
 const playsModel = mongoose.model('play');
 
 const playsList = function (req, res) {
-    res
-        .status(200)
-        .json({'play' : 'won'});
+    playsModel.find({}, function (err, plays) {
+        if (err) {
+            res.status(404).json(err);
+        }
+        else {
+
+            res.status(200).json(plays);
+        }
+
+    });
 
 };
 
 const addPlay = function (req, res) {
 
-    res.status(201).json({"log a play" : "in progress"});
+    playsModel.create(req.body, function(err, newPlay) {
+        if(err) {
+            res.status(400).json(err);
+        }
+        else {
+            res.status(201).json(newPlay);
+        }
+    });
 };
 
 module.exports = {
